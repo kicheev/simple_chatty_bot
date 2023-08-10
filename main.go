db3698d870be804a7f8a70a9f387e9808336c5b9
Andrew <kicheevandrey@gmail.com>
2023-08-10 17:59:01 +0300

    Stage 2. Added username prompt

--- main.go
+++ main.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	var name string
+
 	fmt.Println("Hello! My name is Alan.")
 	fmt.Println("I was created in 2023")
+	fmt.Println("Please, remind your name.")
+
+	fmt.Scan(&name)
+
+	fmt.Println("What a great name you have, " + name + "!")
 }
