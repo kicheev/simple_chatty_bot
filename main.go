aa7d3ca385262050bbf133ab01aa09cc2d5c210f
Andrew <kicheevandrey@gmail.com>
2023-08-11 16:26:47 +0300

    Stage 3. Added age guesser

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	var name string
-
 	fmt.Println("Hello! My name is Alan.")
 	fmt.Println("I was created in 2023")
 	fmt.Println("Please, remind your name.")
 
+	var name string
 	fmt.Scan(&name)
 
 	fmt.Println("What a great name you have, " + name + "!")
+	fmt.Println("Let me guess your age.")
+	fmt.Println("Enter remainders of dividing your age by 3, 5 and 7.")
+
+	var remainder3, remainder5, remainder7 int
+	var age int
+	fmt.Scan(&remainder3, &remainder5, &remainder7)
+
+	age = (remainder3*70 + remainder5*21 + remainder7*15) % 105
+
+	fmt.Println("Your age is " + strconv.Itoa(age) + "; that's a good time to start programming!")
 }
