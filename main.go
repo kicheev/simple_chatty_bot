dded75c0fb24446b471028c4e25e01927b539c26
Andrew <kicheevandrey@gmail.com>
2023-08-16 01:15:13 +0300

    Stage 4. Added the ability to count

--- main.go
+++ main.go
@@ -17,11 +17,20 @@ func main() {
 	fmt.Println("Let me guess your age.")
 	fmt.Println("Enter remainders of dividing your age by 3, 5 and 7.")
 
-	var remainder3, remainder5, remainder7 int
-	var age int
-	fmt.Scan(&remainder3, &remainder5, &remainder7)
+	var rem3, rem5, rem7, age int
+	fmt.Scan(&rem3, &rem5, &rem7)
 
-	age = (remainder3*70 + remainder5*21 + remainder7*15) % 105
+	age = (rem3*70 + rem5*21 + rem7*15) % 105
 
 	fmt.Println("Your age is " + strconv.Itoa(age) + "; that's a good time to start programming!")
+	fmt.Println("Now I will prove to you that I can count to any number you want.")
+
+	var num int
+	fmt.Scan(&num)
+
+	for i := 0; i <= num; i++ {
+		fmt.Println(strconv.Itoa(i) + " !")
+	}
+
+	fmt.Println("Completed, have a nice day!")
 }
