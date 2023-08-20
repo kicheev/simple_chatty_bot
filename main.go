1643169ff6fd76319baadbcdefcacde344422a07
Andrew <kicheevandrey@gmail.com>
2023-08-20 15:49:18 +0300

    (Project completed) Stage 5. Did functional decomposition, added a quiz

--- main.go
+++ main.go
@@ -5,32 +5,78 @@ import (
 	"strconv"
 )
 
-func main() {
-	fmt.Println("Hello! My name is Alan.")
-	fmt.Println("I was created in 2023")
-	fmt.Println("Please, remind your name.")
+func greet(name, year string) {
+	fmt.Println("Hello! My name is " + name + ".")
+	fmt.Println("I was created in " + year + ".")
+}
 
+func showName() {
 	var name string
-	fmt.Scan(&name)
 
+	fmt.Println("Please, remind your name.")
+	fmt.Scan(&name)
 	fmt.Println("What a great name you have, " + name + "!")
+}
+
+func guessAge() {
+	var rem3, rem5, rem7, age int
+
 	fmt.Println("Let me guess your age.")
 	fmt.Println("Enter remainders of dividing your age by 3, 5 and 7.")
-
-	var rem3, rem5, rem7, age int
 	fmt.Scan(&rem3, &rem5, &rem7)
 
 	age = (rem3*70 + rem5*21 + rem7*15) % 105
-
 	fmt.Println("Your age is " + strconv.Itoa(age) + "; that's a good time to start programming!")
-	fmt.Println("Now I will prove to you that I can count to any number you want.")
+}
 
+func count() {
 	var num int
-	fmt.Scan(&num)
 
+	fmt.Println("Now I will prove to you that I can count to any number you want.")
+	fmt.Scan(&num)
 	for i := 0; i <= num; i++ {
 		fmt.Println(strconv.Itoa(i) + " !")
 	}
+}
 
-	fmt.Println("Completed, have a nice day!")
+func startQuiz() {
+	questionOne := func() {
+		var userInput int
+		question := "Why do we use methods?"
+		answerOne := "1. To repeat a statement multiple times."
+		answerTwo := "2. To decompose a program into several small subroutines."
+		answerThree := "3. To determine the execution time of a program."
+		answerFour := "4. To interrupt the execution of a program."
+
+		answers := [4]string{answerOne, answerTwo, answerThree, answerFour}
+		fmt.Println(question)
+		for _, value := range answers {
+			fmt.Println(value)
+		}
+
+		for {
+			fmt.Scan(&userInput)
+			if userInput == 2 {
+				break
+			} else {
+				fmt.Println("Please, try again.")
+			}
+		}
+	}
+
+	fmt.Println("Let's test your programming knowledge.")
+	questionOne()
+}
+
+func sayGoodbye() {
+	fmt.Println("Congratulations, have a nice day!")
+}
+
+func main() {
+	greet("Alan", "2023")
+	showName()
+	guessAge()
+	count()
+	startQuiz()
+	sayGoodbye()
 }
